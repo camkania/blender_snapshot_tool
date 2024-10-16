112f56fb0e22802bbaac4f277874f652da1c51af
Cameron Kania <camkania@gmail.com>
2024-10-16 00:30:36 -0400

    Update utils.py
    Typo and a removal of a default to true

--- utils.py
+++ utils.py
@@ -66,7 +66,7 @@ def set_active_collection(layer_collection, collection_name):
             return True
     return False
 
-def combine_snapshots(keep_seperated_meshes=True):
+def combine_snapshots(keep_separated_meshes):
     snapshot_collection = bpy.data.collections.get("Snapshot_Meshes")
     
     if not snapshot_collection:
